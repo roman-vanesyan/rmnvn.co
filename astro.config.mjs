d9b6463d1acad86e392b435276789d3fd8b20008
Roman Vanesyan <roman@vanesyan.com>
2022-12-03 04:26:34 +0200

    fix: specify astro output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/edge-functions';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://rmnvn.co',
+  integrations: [tailwind(), netlify()],
+  output: 'server',
+  adapter: netlify({})
 });
