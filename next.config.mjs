cef222082cdfd2ed8a53c4af17f26b938a824b4f
Roman Vanesyan <roman@vanesyan.com>
2023-03-26 20:09:27 +0300

    fix: redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,61 @@
+const redirects = [
+  {
+    source: '/twitter',
+    destination: 'https://twitter.com/roman_vanesyan',
+    statusCode: 301
+  },
+  {
+    source: '/github',
+    destination: 'https://github.com/roman-vanesyan',
+    statusCode: 301
+  },
+  {
+    source: '/blog',
+    destination: 'https://romanvanesyan.com',
+    statusCode: 301
+  },
+  {
+    source: '/unsplash',
+    destination: 'https://unsplash.com/@roman_vanesyan',
+    statusCode: 301
+  },
+  {
+    source: '/instagram',
+    destination: 'https://instagram.com/roman_vanesyan',
+    statusCode: 301
+  },
+  {
+    source: '/linkedin',
+    destination: 'https://www.linkedin.com/in/roman-vanesyan',
+    statusCode: 301
+  },
+  {
+    source: '/twitch',
+    destination: 'https://twitch.tv/roman_vanesyan',
+    statusCode: 301
+  },
+  {
+    source: '/youtube',
+    destination: 'https://youtube.com/@roman_vanesyan',
+    statusCode: 301
+  },
+  {
+    source: '/onlyfans',
+    destination: 'https://youtu.be/dQw4w9WgXcQ',
+    statusCode: 301
+  }
+];
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   experimental: {
-    appDir: true,
+    appDir: true
+  },
+
+  async redirects() {
+    return redirects;
   }
-}; 
+};
 
 export default config;
